c267d4fea9f9437bdd7092f7a861bc2d4e36d8da
황원태 <amlk31255@gmail.com>
2022-08-04 23:54:33 +0900

    learn: keys, if render, dynamic data

--- react-complete-guide/src/components/NewExpense/NewExpense.js
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpensesForm from "./ExpensesForm";
 
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHander = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpensesForm onSaveExpenseData={saveExpenseDataHander} />
+      {isEditing ? (
+        <ExpensesForm
+          onSaveExpenseData={saveExpenseDataHander}
+          onCancel={stopEditingHandler}
+        />
+      ) : (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
     </div>
   );
 };
